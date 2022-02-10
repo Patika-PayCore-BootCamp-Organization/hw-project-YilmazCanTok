3e2c43be41799ea9bf696ad3448e15610f667090
Yilmaz Can Tok <yilmazcantok89@gmail.com>
2022-02-10 23:30:56 +0300

    Revert "Revert "second commit""
    This reverts commit 5ed220e02b1c3fdc0d44d62e8e9f6943bc319e51.

--- src/main/java/repository/DormitoryRepository.java
+++ src/main/java/repository/DormitoryRepository.java
@@ -1,2 +1,8 @@
-package repository;public interface DormitoryRepository {
+package repository;
+import model.Dormitory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DormitoryRepository extends JpaRepository<Dormitory, Integer> {
 }
